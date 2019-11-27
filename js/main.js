f1eb1614a1e05201a0312eef868b9005214207f3
Cyril Vandenberghe <cyril.vandenberghe.pro@gmail.com>
2019-11-27 14:51:59 +0100

    fix: remove scroll when no portfolio

--- js/main.js
+++ js/main.js
@@ -149,12 +149,14 @@
 
       // Reveal Item on Scroll
       function revealItem($container, $item) {
-        if ($scrollTop > $container.offset().top - $windowHeight / 1.3) {
-          $item.each(function(i) {
-            setTimeout(function() {
-              $item.eq(i).addClass("is-showing");
-            }, 150 * (i + 1));
-          });
+        if ($container.offset()) {
+          if ($scrollTop > $container.offset().top - $windowHeight / 1.3) {
+            $item.each(function(i) {
+              setTimeout(function() {
+                $item.eq(i).addClass("is-showing");
+              }, 150 * (i + 1));
+            });
+          }
         }
       }
 
